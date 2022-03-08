cd38864f796a566a673aebe2f0d795760971ff9b
suea724 <suea724@gmail.com>
2022-03-08 20:52:06 +0900

    # If문 : 주사위 세개

---
+++ BOJ/step/step2/2480.py
@@ -0,0 +1,10 @@
+# 주사위 세개
+A, B, C = map(int, input().split())
+if A == B and B == C:
+    print(10000 + A * 1000)
+elif A == B or A == C:
+    print(1000 + A * 100)
+elif B == C:
+    print(1000 + B * 100)
+else :
+    print(max(A, B, C) * 100)
